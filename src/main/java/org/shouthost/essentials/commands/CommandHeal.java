1259d9419422db6b34101b54835ceb862a9bfa60
Darius Clark <darius.clark@shouthost.org>
2014-05-28 09:57:06 -0400

    Added CommandHeal

--- src/main/java/org/shouthost/essentials/commands/CommandHeal.java
+++ src/main/java/org/shouthost/essentials/commands/CommandHeal.java
@@ -1,6 +1,12 @@
 package org.shouthost.essentials.commands;
 
+import forgeperms.api.ForgePermsAPI;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
 
 import java.util.List;
 
@@ -10,12 +16,12 @@ import java.util.List;
 public class CommandHeal extends ECommandBase {
 	@Override
 	public String getPermissionNode() {
-		return null;
+		return "essentials.heal";
 	}
 
 	@Override
 	public boolean canConsoleUseCommand() {
-		return false;
+		return true;
 	}
 
 	@Override
@@ -25,16 +31,33 @@ public class CommandHeal extends ECommandBase {
 
 	@Override
 	public String getCommandName() {
-		return null;
+		return "heal";
 	}
 
 	@Override
 	public String getCommandUsage(ICommandSender iCommandSender) {
-		return null;
+		return "/heal [<player>]";
 	}
 
 	@Override
 	public void processCommand(ICommandSender iCommandSender, List<String> args) {
-
+		EntityPlayer p = (EntityPlayer) iCommandSender;
+		if(args.isEmpty()){
+			if(!(iCommandSender instanceof EntityPlayer)) return;
+			p.setHealth(200);
+			p.addChatMessage(new ChatComponentText(EnumChatFormatting.GREEN+"You have been healed"));
+			return;
+		}else{
+			if(!ForgePermsAPI.permManager.canAccess(p.getDisplayName(), p.worldObj.getWorldInfo().getWorldName(), getPermissionNode()+".self")){
+				p.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "You do not have permission to this action"));
+				return;
+			}
+			EntityPlayerMP target = MinecraftServer.getServer().getConfigurationManager().getPlayerForUsername(args.get(0));
+			if(target == null){
+				p.addChatMessage(new ChatComponentText(EnumChatFormatting.RED+"Player does not exist"));
+				return;
+			}
+			target.setHealth(200);
+		}
 	}
 }
