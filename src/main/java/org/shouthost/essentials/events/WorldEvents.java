bb8de68fd89314d5994ecf9a85b038ea8edc93b5
Darius Clark <darius.clark@shouthost.org>
2014-05-28 10:00:46 -0400

    Updated WorldEvents This *should* go through the player list and save any data. Will need to rewrite though.

--- src/main/java/org/shouthost/essentials/events/WorldEvents.java
+++ src/main/java/org/shouthost/essentials/events/WorldEvents.java
@@ -2,6 +2,9 @@ package org.shouthost.essentials.events;
 
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.WorldEvent;
 import org.shouthost.essentials.core.Essentials;
@@ -9,7 +12,7 @@ import org.shouthost.essentials.json.players.Players;
 import org.shouthost.essentials.utils.config.Player;
 
 import java.io.File;
-import java.util.Iterator;
+import java.util.*;
 
 public class WorldEvents {
 	public WorldEvents(){
@@ -19,11 +22,14 @@ public class WorldEvents {
 
 	@SubscribeEvent
 	public void onWorldSave(WorldEvent.Save event){
-		Iterator<Players> list = Essentials.playerList.iterator();
-		while(list.hasNext()){
-			Player.SavePlayer(list.next());
+		//Get the list of players on the server
+		List<EntityPlayer> list = MinecraftServer.getServer().getConfigurationManager().playerEntityList;
+		for(EntityPlayer p: list){
+				p.addChatMessage(new ChatComponentText("Saving your data"));
+				Player player = new Player(p);
+				player.save();
 		}
 	}
-
-
 }
+
+
