a6f7bae132a8608f9adeca5ce5edf78c9e706383
Darius Clark <darius.clark@shouthost.org>
2014-05-28 07:09:40 -0400

    Updated CommandHome and CommandSethome. Fixed issue with homes not saving and being checked like it should .

--- src/main/java/org/shouthost/essentials/commands/CommandHome.java
+++ src/main/java/org/shouthost/essentials/commands/CommandHome.java
@@ -1,13 +1,23 @@
 package org.shouthost.essentials.commands;
 
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+import org.shouthost.essentials.json.players.Homes;
+import org.shouthost.essentials.json.players.Players;
+import org.shouthost.essentials.utils.config.Player;
 
 import java.util.List;
 
 public class CommandHome extends ECommandBase {
 	@Override
 	public String getPermissionNode() {
-		return null;
+		return "essentials.home";
 	}
 
 	@Override
@@ -22,7 +32,7 @@ public class CommandHome extends ECommandBase {
 
 	@Override
 	public String getCommandName() {
-		return null;
+		return "home";
 	}
 
 	@Override
@@ -32,6 +42,40 @@ public class CommandHome extends ECommandBase {
 
 	@Override
 	public void processCommand(ICommandSender iCommandSender, List<String> args) {
+		EntityPlayer p = (EntityPlayer) iCommandSender;
+		Player player = new Player(p);
+		if(p.isRiding()){
+			player.sendMessage(EnumChatFormatting.RED+"You are not allowed to go home while riding an entity");
+			return;
+		}
+		if(args.isEmpty()) {
+			Homes home = player.getHome("home");
+			if (home == null) {
+				player.sendMessage(EnumChatFormatting.RED + "You have no set home!");
+				return;
+			}
 
+			WorldServer world = MinecraftServer.getServer().worldServerForDimension(home.getWorld());
+			if (world == null) {
+				player.sendMessage(EnumChatFormatting.RED+"The world your home is set in does not exist");
+				return;
+			}
+			player.teleportTo(world, home.getX(), home.getY(), home.getZ());
+			return;
+		} else if (!args.isEmpty()){
+				//check to see if world exist before attempting to teleport
+				Homes home = player.getHome(args.get(0));
+				if (home == null) {
+					player.sendMessage(EnumChatFormatting.RED + "You have no set home!");
+					return;
+				}
+				WorldServer world = MinecraftServer.getServer().worldServerForDimension(home.getWorld());
+				if (world == null) {
+					player.sendMessage(EnumChatFormatting.RED+"The world your home is set in does not exist");
+					return;
+				}
+				player.teleportTo(world, home.getX(), home.getY(), home.getZ());
+				return;
+		}
 	}
 }
--- src/main/java/org/shouthost/essentials/commands/CommandSethome.java
+++ src/main/java/org/shouthost/essentials/commands/CommandSethome.java
@@ -1,16 +1,19 @@
 package org.shouthost.essentials.commands;
 
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
+import org.shouthost.essentials.json.players.Homes;
+import org.shouthost.essentials.json.players.Players;
+import org.shouthost.essentials.utils.config.Player;
 
 import java.util.List;
 
-/**
- * Created by Darius on 5/20/2014.
- */
 public class CommandSethome extends ECommandBase {
 	@Override
 	public String getPermissionNode() {
-		return null;
+		return "essentials.home.set";
 	}
 
 	@Override
@@ -24,13 +27,8 @@ public class CommandSethome extends ECommandBase {
 	}
 
 	@Override
-	public boolean canUseWithoutPermission() {
-		return false;
-	}
-
-	@Override
 	public String getCommandName() {
-		return null;
+		return "sethome";
 	}
 
 	@Override
@@ -40,6 +38,26 @@ public class CommandSethome extends ECommandBase {
 
 	@Override
 	public void processCommand(ICommandSender iCommandSender, List<String> args) {
+		EntityPlayer p = (EntityPlayer) iCommandSender;
+		Player player = new Player(p);
+		if(args.isEmpty()){
+			player.setHome("home", (int)p.posX, (int)p.posY, (int)p.posZ);
+			player.save();
+			p.addChatMessage(new ChatComponentText(EnumChatFormatting.GREEN+"Your home have been set!"));
+			return;
+		}else{
+			//find if home exist
+			Homes home = player.getHome(args.get(0));
+			if(home != null) {
+				p.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Home '" + args.get(0) + "' already exist!"));
+				return;
+			}else if(home == null){
+				player.setHome(args.get(0), (int)p.posX, (int)p.posY, (int)p.posZ);
+				player.save();
+				p.addChatMessage(new ChatComponentText(EnumChatFormatting.GREEN+"Your home have been set!"));
+				return;
+			}
 
+		}
 	}
 }
