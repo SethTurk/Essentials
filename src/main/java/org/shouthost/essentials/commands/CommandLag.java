6c0995f4f38b07f2051b7205bc35edb01746a105
Darius Clark <darius.clark@shouthost.org>
2014-05-28 10:01:51 -0400

    Updated CommandLag Implemented methods for checking TPS, and memory. Will need to finish this so it can have a check per world.

--- src/main/java/org/shouthost/essentials/commands/CommandLag.java
+++ src/main/java/org/shouthost/essentials/commands/CommandLag.java
@@ -1,7 +1,13 @@
 package org.shouthost.essentials.commands;
 
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.world.World;
+import org.shouthost.essentials.utils.config.Player;
 
+import java.lang.management.ManagementFactory;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,6 +35,17 @@ public class CommandLag extends ECommandBase {
 
 	@Override
 	public void processCommand(ICommandSender iCommandSender, List<String> args) {
+		Player player = new Player(iCommandSender);
+		//Uptime
+		player.sendMessage("Uptime: "+ ManagementFactory.getRuntimeMXBean().getStartTime());//Will implement a parser for uptime
+
+		//Memory
+		long maxmem = Runtime.getRuntime().totalMemory() / 1024L / 1024L;
+		long freemem = Runtime.getRuntime().freeMemory() / 1024L / 1024L;
+		long memuse = maxmem - freemem / 1024L / 1024L;
+		player.sendMessage("Maximum memory: "+maxmem+" MB.");
+		player.sendMessage("Used Memory: "+memuse+" MB.");
+		player.sendMessage("Free memory: "+freemem+" MB.");
 
 	}
 
@@ -50,5 +67,20 @@ public class CommandLag extends ECommandBase {
 	@Override
 	public boolean canUseWithoutPermission() {
 		return false;
+	}
+
+	private double getWorldTPS(World world){
+		double worldTickTime = mean(MinecraftServer.getServer().worldTickTimes.get(world.provider.dimensionId)) * 1.0E-6D;
+		return Math.min(1000.0 / worldTickTime, 20);
+	}
+
+	private static long mean(long[] values) {
+
+		long sum = 0l;
+		for (long v : values) {
+			sum += v;
+		}
+
+		return sum / values.length;
 	}
 }
